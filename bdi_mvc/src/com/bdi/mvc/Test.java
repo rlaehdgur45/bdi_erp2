18f5bbff87e2e6d1268877bc647ca9ed298d33de
KOITT <KOITT@KOITT-PC>
2018-09-04 21:33:53 +0900

    asdsad

---
+++ bdi_mvc/src/com/bdi/mvc/Test.java
@@ -0,0 +1,50 @@
+package com.bdi.mvc;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class FileItem{
+	String name;
+	String string;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getString() {
+		return string;
+	}
+
+	public void setString(String string) {
+		this.string = string;
+	}
+}
+public class Test {
+	public static void main (String[] args) {
+		List<FileItem>  fList = new ArrayList<FileItem>();
+		FileItem fi = new FileItem();
+		fi.setName("gcName");
+		fi.setString("카트라이더");
+		fList.add(fi);
+		
+		fi= new FileItem();
+		fi.setName("gcPrice");
+		fi.setString("10000");
+		fList.add(fi);
+		
+		
+		
+		Map<String,String> param = new HashMap<String,String>();
+		for(FileItem ff:fList) {
+			param.put(ff.getName(), ff.getString());
+			
+		}
+		System.out.println(param.get("gcName"));
+	}
+}
---
+++ bdi_mvc/src/com/bdi/mvc/common/UploadFile.java
@@ -0,0 +1,56 @@
+package com.bdi.mvc.common;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+public class UploadFile {
+
+	private static final String UPLOAD_PATH = "C:\\bdi_study\\workspace\\git\\bdi_mvc\\WebContent";
+	
+	
+	public static Map<String,String> saveFileList(HttpServletRequest req) throws ServletException{
+		if(!ServletFileUpload.isMultipartContent(req)) {
+			throw new ServletException("폼형식이 파일 업로드에 적합하지 않습니다.");
+		}
+		DiskFileItemFactory diFactory = new DiskFileItemFactory();
+		diFactory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+		diFactory.setSizeThreshold(1024*1024*5);
+		
+		ServletFileUpload sfu = new ServletFileUpload(diFactory);
+		sfu.setHeaderEncoding("urf-8");
+		sfu.setSizeMax(1024*1024*100);
+		sfu.setFileSizeMax(1024*1024*50);
+		try {
+			List<FileItem> fList = sfu.parseRequest(req);
+			if(fList==null||fList.size()==0){
+				throw new ServletException("업로드할 파일이 없습니다!!");
+				
+			}
+			Map<String,String> params = new HashMap<String,String>();
+			for(FileItem fi : fList) {
+				if(!fi.isFormField()) {
+					String fEndName = fi.getName().substring(fi.getName().lastIndexOf("."));
+					String fileName = File.separator + "files" + File.separator + System.currentTimeMillis() + fEndName;
+					String filePath = UPLOAD_PATH + File.separator + fileName;
+					File sFile = new File(filePath);
+					fi.write(sFile);
+					params.put(fi.getFieldName(), fileName);
+				}else {
+					params.put(fi.getFieldName(),fi.getString("utf-8"));
+				}
+			}
+			return params;
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}
+	}
+}
